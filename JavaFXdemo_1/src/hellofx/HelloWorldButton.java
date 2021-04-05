813fb3d0f56b3afc3f506d15f816cb61c6c66551
rkjc <richardkjcross@gmail.com>
2021-04-05 02:18:11 -0700

    added the JavaFXdemo_1 project

---
+++ JavaFXdemo_1/src/hellofx/HelloWorldButton.java
@@ -0,0 +1,39 @@
+package hellofx;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class HelloWorldButton extends Application {
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Hello World! window title");
+		Text tx = new Text("Look at this awesome text!.Hidden behind the button");
+		Button btn = new Button();
+		btn.setText("Say 'Hello World'");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Hello to the World!");
+			}
+		});
+
+		StackPane root = new StackPane();	
+		root.getChildren().add(tx);
+		root.getChildren().add(btn);	
+		Scene sc = new Scene(root, 400, 250);
+		primaryStage.setScene(sc);
+		primaryStage.show();
+	}
+
+}
